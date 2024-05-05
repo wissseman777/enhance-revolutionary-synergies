1119e5bb8788646e0608af212603c872f2c360e3
wissseman777 <161206102+wissseman777@users.noreply.github.com>
2024-05-05 17:40:17 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,67 +1,24 @@
-        print(f"Random Number: {item}")
-    main()
+            data[i] = (int) (Math.random() * 100) + 1;
 
-    return data
-
-    for item in data:
-def generate_random_data():
-def main():
-if __name__ == "__main__":
-
-
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-import random        for (int item : data) {
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
         }
+        for (int item : data) {
+
+    }
+        int[] data = new int[10];
 }
+        }
+    public static void main(String[] args) {    }
+public class RandomDataGenerator {
     public static void main(String[] args) {
             System.out.println("Random Number: " + item);
-
-    }
-        for (int i = 0; i < 10; i++) {
             data[i] = (int) (Math.random() * 100) + 1;
-        }
-public class RandomDataGenerator {
 
-        int[] data = new int[10];def generate_random_data():
-def main():
-
-    for item in data:
-    main()
-
-    data = generate_random_data()
-
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-
-    return data
-if __name__ == "__main__":function generateRandomData() {
-    });
-    const data = [];
-    }
-
-    return data;
-
-}
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
-        console.log("Random Number: " + item);
-    data.forEach(item => {
-}
-    for (let i = 0; i < 10; i++) {
-main();
-
-    const data = generateRandomData();            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
-        }
-    public static void main(String[] args) {
-            data[i] = (int) (Math.random() * 100) + 1;
         for (int item : data) {
-        int[] data = new int[10];
-
 }
-    }
-        for (int i = 0; i < 10; i++) {
-
         }
+        for (int i = 0; i < 10; i++) {
+        }
+        int[] data = new int[10];
